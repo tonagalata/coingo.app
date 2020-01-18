d1a7fb3f15552c1a7dd351a3044ffccff3397611
Galata Tona <tonagalata+2@gmail.com>
2020-01-18 15:24:43 -0500

    adds group admin view

--- routes/groupAdmins.js
+++ routes/groupAdmins.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const membersCtrl = require('../controllers/members');
 const groupsCtrl = require('../controllers/groups')
 const groupAdminCtrl = require('../controllers/groupAdmins');
+const siteAdminCtrl = require('../controllers/admins');
 
 
 
+router.get('/admin/:id/', membersCtrl.isLoggedIn, siteAdminCtrl.index);
 router.get('/admin/:id/', membersCtrl.isLoggedIn, groupAdminCtrl.index);
 router.get('/admin/show', membersCtrl.isLoggedIn, groupAdminCtrl.show);
 // router.post('/tickets', ticketsCtrl.create);
